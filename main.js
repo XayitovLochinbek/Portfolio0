e2c238a019b48f17b45eb0b76ca95e68ac94cee3
XayitovLochinbek <xayitovlochinbek2008@gmail.com>
2023-10-12 13:56:51 +0500

    yangilanish

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const elBtn = document.querySelector('.menu-btn')
 const elNavigation = document.querySelector('.navigation')
 const elNavigationItems = document.querySelectorAll('.navigation a')
-
+const elBtn2 = document.querySelector('.menu-btn2')
 elBtn.addEventListener('click' , ()=>{
     elBtn.classList.toggle('active')
     elNavigation.classList.toggle('active')
@@ -13,3 +13,7 @@ elNavigationItems.forEach(navItem => {
     })
 })
 
+elBtn2.addEventListener('click' , ()=>{
+    elBtn.classList.toggle('active')
+    elNavigation.classList.toggle('active')
+})
